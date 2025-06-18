62f8556c5ffe511e7526450cbed7306777d83f80
theskillfulnoob <theskillfulnoob2002@gmail.com>
2025-06-18 11:20:01 +0800

    prof viz

--- bowling_dashboard_app.py
+++ bowling_dashboard_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from ocr_ui import session_input_tab, compute_bowling_stats
 from stats_ui import stats_tabs
 from regression_ui import regression_tabs
-# from prof_ui import professional_tab
+from prof_ui import professional_tab
 from sheets import push_session_data, push_ground_truth, sync_aggregates_from_full
 from data import load_sessions, filter_sessions
 import pandas as pd
@@ -14,7 +14,7 @@ sync_aggregates_from_full()
 tabs = st.tabs(["➕ Process Session",
                 "📈 Stats",
                 "📊 Regression", 
-                #"🏅 Professional"
+                "🏅 Professional"
                 ])
 with tabs[0]:
     session_input_tab()
@@ -25,5 +25,5 @@ with tabs[1]:
 with tabs[2]:
     regression_tabs()
 
-#with tabs[3]:
-#    professional_tab()
+with tabs[3]:
+    professional_tab()
